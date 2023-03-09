e58916cbd9a06caa6f0af51d23ecfd6050951913
Thormer790 <agustin.mazza@gmail.com>
2023-03-08 21:01:26 -0300

    Primera escritura

---
+++ JS/final.js
@@ -0,0 +1,16 @@
+document.getElementById("boton1").onclick = function() {
+    location.href="https://twitter.com/?lang=en"
+}
+document.getElementById("boton2").onclick = function() {
+    location.href="https://www.facebook.com/"
+}
+document.getElementById("boton3").onclick = function() {
+    location.href="https://www.linkedin.com/"
+}
+document.getElementById("boton4").onclick = function() {
+    location.href="https://www.linkedin.com/"
+}
+document.getElementById("boton5").onclick = function() {
+    location.href="https://www.linkedin.com/"
+}
+
